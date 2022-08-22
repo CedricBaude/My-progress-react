1d18c222fcff40157f085812d2c39dfdb1a7b091
CedricBaude <baude.cedric.perso@gmail.com>
2022-08-22 16:29:22 +0200

    maj progress

---
+++ src/pages/TechnoList.js
@@ -0,0 +1,8 @@
+export default function TechnoList(){
+    return(
+        <div className="techno-list" >
+            <h1>All Tecnos</h1>
+            <p>TODO display all technos added</p>
+        </div>
+    );
+}
