58707fd0deca259b98e8ffbde18866eb2490f271
CedricBaude <baude.cedric.perso@gmail.com>
2022-08-25 15:46:50 +0200

    maj

--- src/pages/TechnoAdd.js
+++ src/pages/TechnoAdd.js
@@ -1,22 +1,36 @@
+import { useState } from 'react';
+
 export default function TechnoAdd(props) {
+    const [techno, setTechno] = useState({
+        technomane: '',
+        technocategory: '',
+        technodescription: ''
+    });
+
     const { handleAddTechno } = props;
-    const techno = {
-        name: 'React',
-        category: 'front',
-        description: 'Learn React'
-    };
+
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        handleAddTechno(techno);
+    }
+
+    function handleChange(evt) {
+        const { name, value } = evt.target;
+        setTechno({ ...techno, [name]: value });
+    }
     return (
         <div className="techno-add">
             <h1>Add a Techno</h1>
             <div>
-                <form onSubmit={(evt) => handleAddTechno(evt)}>
-                    <label htmlFor="techno-name">Name</label>
+                <form onSubmit={(evt) => handleSubmit(evt)}>
+                    <label htmlFor="technoname">Name</label>
                     <br />
-                    <input type="text" name="techno-name" id="techno-name" />
+                    <input type="text" name="technoname" id="technoname" value={techno.technomane} onChange={(evt) => handleChange(evt)} />
+                    <input type="text" name="technoname" id="technoname" value={techno.technoname} onChange={(evt) => handleChange(evt)} />
                     <br />
-                    <label htmlFor="techno-category">Category:</label>
+                    <label htmlFor="technocategory">Category:</label>
                     <br />
-                    <select name="techno-category" id="techno-category">
+                    <select name="technocategory" id="technocategory" value={techno.technocategory} onChange={(evt) => handleChange(evt)}>
                         <option value="">Select a category</option>
                         <option value="front">Front</option>
                         <option value="back">Back</option>
@@ -24,11 +38,11 @@ export default function TechnoAdd(props) {
                         <option value="other">Other</option>
                     </select>
                     <br />
-                    <label htmlFor="techno-description">Description:</label>
+                    <label htmlFor="technodescription">Description:</label>
                     <br />
-                    <textarea name="techno-description" id="techno-description" cols="30" rows="10" ></textarea>
+                    <textarea name="technodescription" id="technodescription" cols="30" rows="10" ></textarea>
                     <br />
-                    <input type="submit" value="Add Techno" />
+                    <input type="submit" value="Add Techno" className="btn" />
                 </form>
             </div>
         </div>
