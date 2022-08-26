211728d1461dd08525cbaa9c2699beaabda81609
CedricBaude <baude.cedric.perso@gmail.com>
2022-08-26 17:15:39 +0200

    update useLocalStorage

--- src/App.js
+++ src/App.js
@@ -4,18 +4,32 @@ import Menu from './components/Menu';
 import TechnoAdd from './pages/TechnoAdd';
 import TechnoList from './pages/TechnoList';
 import { Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { useLocalStorage } from './hooks/useLocalStorage';
 
 function App() {
   const [technos, setTechnos] = useState([]);
+  const STORAGE_KEY = 'technos';
+  const [storedTechnos, setStoredTechnos] = useLocalStorage(STORAGE_KEY, []);
+
+  useEffect(() => {
+    console.log('useEffetc with []');
+    setTechnos(storedTechnos);
+  }, []);
+
+  useEffect(() => {
+    console.log('useEffetc with [technos]');
+    setStoredTechnos(technos);
+  }, [technos]);
 
   function handleAddTechno(techno) {
+    console.log('handleAddTechno', techno);
     setTechnos([...technos, { ...techno, technoid: uuidv4() }]);
   }
 
-  function handleDeleteTechno(id){
-setTechnos(technos.filter((tech => tech.technoid !== id)))
+  function handleDeleteTechno(id) {
+    setTechnos(technos.filter((tech => tech.technoid !== id)))
   }
 
   return (
