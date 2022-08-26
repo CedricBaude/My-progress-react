aae1d3ccd8ac28c02f5c87bfdb0b82ae90580b20
CedricBaude <baude.cedric.perso@gmail.com>
2022-08-26 15:39:07 +0200

    update refacto component

--- src/pages/TechnoList.js
+++ src/pages/TechnoList.js
@@ -1,3 +1,5 @@
+import TechnoItem from "../components/TechnoItem";
+
 export default function TechnoList(props) {
     const { technos } = props;
     return (
@@ -5,15 +7,9 @@ export default function TechnoList(props) {
             <h1>All Technos</h1>
             <div>
                 {
-                    technos.map(techno =>(
-                        <div key={techno.technoid}>
-                            <h2>{techno.technoname}</h2>
-                            <h3>Category</h3>
-                            <p>{techno.technocategory}</p>
-                            <h3>Description</h3>
-                            <p>{techno.technodescription}</p>
-                        </div>
-                    ) )
+                    technos.map(techno => (
+                        <TechnoItem techno={techno} key={techno.technoid} />
+                    ))
                 }
             </div>
         </div>
